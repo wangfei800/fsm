41a8c97577799c67ecee5dde565d6d19e9adccb2
Thomas Wang <thomas.wang@uniuini.com>
2025-11-07 15:24:25 -0800

    Add unit test

--- UnitTestFsm.php
+++ UnitTestFsm.php
@@ -51,4 +51,65 @@ class UnitTestFsm extends TestCase
         $this->assertStringContainsString('transitionFunc should be callable', $generator->getError());
     }
 
+    public function testFsm()
+    {
+        function transitionFunc($state, $var)
+        {
+            static $transitionMatrix = [
+                'S0' => [
+                    0 => 'S0',
+                    1 => 'S1',
+                ],
+                'S1' => [
+                    0 => 'S2',
+                    1 => 'S0',
+                ],
+                'S2' => [
+                    0 => 'S1',
+                    1 => 'S2',
+                ],
+
+            ];
+
+            if (!isset($transitionMatrix[$state][$var])) {
+                throw new Exception("Cannot transition from state: $state on: $var");
+            } else {
+                return $transitionMatrix[$state][$var];
+            }
+        };
+
+        $States         = ['S0', 'S1', 'S2'];
+        $inputSet       = [0, 1];
+        $finalStates    = ['S0', 'S1', 'S2'];
+        $initialState   = 'S0';
+        $fsmGenerator   = new FsmGenerator();
+        $fsmParams      = new FsmParams($States, $inputSet, $initialState, $finalStates, 'transitionFunc');
+
+        $generator = new FsmGenerator();
+        $fsm = $generator->generateFsm($fsmParams);
+
+        // result should be S1 for 1
+        $result = $fsm->run('1');
+        if ($result && is_array($result)) {
+            $this->assertEquals('S1', $result['state']);
+        }
+
+        // result should be S2 for 10
+        $result = $fsm->run('10');
+        if ($result && is_array($result)) {
+            $this->assertEquals('S2', $result['state']);
+        }
+
+        // result should be S2 for 101
+        $result = $fsm->run('101');
+        if ($result && is_array($result)) {
+            $this->assertEquals('S2', $result['state']);
+        }
+
+        // result should be S1 for 1010
+        $result = $fsm->run('1010');
+        if ($result && is_array($result)) {
+            $this->assertEquals('S1', $result['state']);
+        }
+    }
 }
